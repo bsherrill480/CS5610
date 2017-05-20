907c5f5b62ee5e0c8b0681ca46b70d422dfc729f
yanchao <870154211@qq.com>
2017-05-19 21:12:15 -0700

    add in index.html

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 /**
  * Created by yanchao on 5/19/17.
  */
+//using express with node js
+var express = require('express');
+
+//initialize app as an express application
+var app = express();
+
+var ipaddress = '127.0.0.1';
+var port      = process.env.PORT || 5000;
+
+app.use(express.static(__dirname+'/public'));
+app.listen(port, ipaddress);
+
+console.log("hello world!");
