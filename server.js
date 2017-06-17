7ce552b90ee07561f56260abefb8af0557ca0434
yanchao <870154211@qq.com>
2017-06-16 21:53:43 -0700

    remove local ip

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ var express = require('express');
 var app = express();
 
 /*var ipaddress = '127.0.0.1';*/
-var port      = process.env.PORT || 5000;
 
 app.use(express.static(__dirname+'/public/assignment'));
-/*app.listen(port, ipaddress);*/
-app.listen(port);
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
 
 console.log("hello world WebServer!");
