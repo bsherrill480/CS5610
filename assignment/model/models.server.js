a43fbf8748c1836d06124af4459275e0d35238c6
yanchao <870154211@qq.com>
2017-07-28 01:10:29 -0700

    mlab

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -2,11 +2,11 @@ module.exports = function(mongoose) {
     var connectionString =  null;
 
     if (process.env.MONGODB_URI) {
-        connectionString = 'mongodb://cs5610-webdev:webdev@ds145892.mlab.com:45892/heroku_pxg85hjc';
+        connectionString = 'mongodb://cs5610:cs5610@ds147821.mlab.com:47821/heroku_h7rf48jn';
     }
     else
     {
-        connectionString = 'mongodb://localhost:27017/cs5610-webdev'
+        connectionString = 'mongodb://localhost:27017/cs5610_webdev'
     }
 
     mongoose.connect(connectionString);
@@ -15,13 +15,13 @@ module.exports = function(mongoose) {
     var userModel = require("./user/user.model.server.js")(mongoose);
     var websiteModel = require("./website/website.model.server.js")(mongoose, userModel);
     var pageModel =  require("./page/page.model.server.js")(mongoose, websiteModel);
-    //var widgetModel = require("./widget/widget.model.server.js")(mongoose, pageModel);
+    var widgetModel = require("./widget/widget.model.server.js")(mongoose, pageModel);
 
     var models = {
         'userModel' : userModel,
         'websiteModel' : websiteModel,
         'pageModel' : pageModel,
-        //'widgetModel' : widgetModel
+        'widgetModel' : widgetModel
     };
 
     return models;
