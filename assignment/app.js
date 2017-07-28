c54e68ff491c8a4efc629a383dfe54f60c641f89
yanchao <870154211@qq.com>
2017-07-28 14:52:45 -0700

    mlab failed

--- assignment/app.js
+++ assignment/app.js
@@ -1,9 +1,13 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cs5610_webdev');
+mongoose.Promise = require('q').Promise;
+
+
 module.exports = function(app){
 
-    var models = require("./model/models.server.js")();
 
-    require("./services/user.service.server.js")(app, models);
-    require("./services/website.service.server.js")(app, models);
-    require("./services/page.service.server")(app, models);
-    require("./services/widget.service.server")(app, models);
+    require("./services/user.service.server.js")(app);
+    require("./services/website.service.server.js")(app);
+    require("./services/page.service.server")(app);
+    require("./services/widget.service.server")(app);
 };
