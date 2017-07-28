4d97ea48c39813e3a384eb351d717e6d211127c2
yanchao <870154211@qq.com>
2017-07-28 14:11:29 -0700

    mlab

--- assignment/app.js
+++ assignment/app.js
@@ -1,10 +1,9 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/cs5610_webdev');
-mongoose.Promise = require('q').Promise;
 module.exports = function(app){
 
-    require("./services/user.service.server.js")(app);
-    require("./services/website.service.server.js")(app);
-    require("./services/page.service.server")(app);
-    require("./services/widget.service.server")(app);
+    var models = require("./model/models.server.js")();
+
+    require("./services/user.service.server.js")(app, models);
+    require("./services/website.service.server.js")(app, models);
+    require("./services/page.service.server")(app, models);
+    require("./services/widget.service.server")(app, models);
 };
